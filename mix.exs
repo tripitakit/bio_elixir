4ebc8d75df4f52f4bff10178d8df790821bbe00c
tripitakit <patrick.demarta@gmail.com>
2019-06-03 13:49:55 +0200

    relax required Elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BioElixir.MixProject do
     [
       app: :bio_elixir,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
